a3e08cc4d7dd87ff0b97f9c9c5ddca09a7a4cc05
JuKu <kuenzel.justin@t-online.de>
2017-03-28 17:00:36 +0200

    added support to draw background image.

--- game/src/main/java/dev/game/spacechaos/game/screen/GameScreen.java
+++ game/src/main/java/dev/game/spacechaos/game/screen/GameScreen.java
@@ -13,11 +13,22 @@ import dev.game.spacechaos.engine.time.GameTime;
  */
 public class GameScreen extends BaseScreen {
 
+    protected static final String BG_IMAGE_PATH = "./data/images/skybox/galaxy/galaxy+X.png";
+
     //background image
     protected Texture bgTexture = null;
 
     @Override protected void onInit(ScreenBasedGame game, AssetManager assetManager) {
-        //
+        //load all neccessary assets
+
+        //load skybox
+        assetManager.load(BG_IMAGE_PATH, Texture.class);
+
+        //wait while assets are loading
+        assetManager.finishLoadingAsset(BG_IMAGE_PATH);
+
+        //get asset
+        this.bgTexture = assetManager.get(BG_IMAGE_PATH, Texture.class);
     }
 
     @Override public void onResume() {
@@ -34,11 +45,12 @@ public class GameScreen extends BaseScreen {
     }
 
     @Override public void draw(GameTime time, SpriteBatch batch) {
-
+        //draw background skybox image
+        batch.draw(this.bgTexture, 0, 0, game.getViewportWidth(), game.getViewportHeight());
     }
 
     @Override public void destroy() {
-
+        assetManager.unload(BG_IMAGE_PATH);
     }
 
 }
