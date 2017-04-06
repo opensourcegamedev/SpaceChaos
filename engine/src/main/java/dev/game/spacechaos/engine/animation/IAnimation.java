dd53551be3d3d52990b9c3618ee78b83fe55b4d5
JuKu <kuenzel.justin@t-online.de>
2017-04-06 16:18:56 +0200

    added support for explosion animations.

---
+++ engine/src/main/java/dev/game/spacechaos/engine/animation/IAnimation.java
@@ -0,0 +1,19 @@
+package dev.game.spacechaos.engine.animation;
+
+/**
+ * Created by Justin on 06.04.2017.
+ */
+public interface IAnimation {
+
+    public void start ();
+
+    public void stop ();
+
+    public boolean isPlaying ();
+
+    /**
+    * reset animation
+    */
+    public void reset ();
+
+}
