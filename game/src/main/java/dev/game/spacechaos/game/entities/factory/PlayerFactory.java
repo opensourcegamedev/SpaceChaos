550f6cff33b16531b520eb070be1ba514fe3ad37
JuKu <kuenzel.justin@t-online.de>
2017-04-07 22:13:10 +0200

    added PlayerFactory.

---
+++ game/src/main/java/dev/game/spacechaos/game/entities/factory/PlayerFactory.java
@@ -0,0 +1,31 @@
+package dev.game.spacechaos.game.entities.factory;
+
+import com.badlogic.gdx.graphics.Texture;
+import dev.game.spacechaos.engine.entity.Entity;
+import dev.game.spacechaos.engine.entity.EntityManager;
+import dev.game.spacechaos.engine.entity.component.PositionComponent;
+import dev.game.spacechaos.engine.entity.component.camera.SmoothFollowCameraComponent;
+import dev.game.spacechaos.engine.entity.component.draw.DrawTextureComponent;
+
+/**
+ * Created by Justin on 07.04.2017.
+ */
+public class PlayerFactory {
+
+    public static Entity createPlayer (EntityManager ecs, float x, float y, Texture texture) {
+        //create new entity
+        Entity player = new Entity(ecs);
+
+        //every entity requires an position component
+        player.addComponent(new PositionComponent(x, y), PositionComponent.class);
+
+        //add texture component to draw texture
+        player.addComponent(new DrawTextureComponent(texture), DrawTextureComponent.class);
+
+        //add follow camera component, so camera is following player
+        player.addComponent(new SmoothFollowCameraComponent(), SmoothFollowCameraComponent.class);
+
+        return player;
+    }
+
+}
