861e53a3c0c2a876c9640bb0a872d97828a2e280
JuKu <kuenzel.justin@t-online.de>
2017-04-10 20:41:31 +0200

    added ProjectileFactory.

---
+++ game/src/main/java/dev/game/spacechaos/game/entities/factory/ProjectileFactory.java
@@ -0,0 +1,16 @@
+package dev.game.spacechaos.game.entities.factory;
+
+import com.badlogic.gdx.graphics.Texture;
+import dev.game.spacechaos.engine.entity.Entity;
+import dev.game.spacechaos.engine.entity.EntityManager;
+
+/**
+ * Created by Justin on 10.04.2017.
+ */
+public class ProjectileFactory {
+
+    public static Entity createProjectile (EntityManager ecs, float x, float y, Texture texture, float moveX, float moveY) {
+        throw new UnsupportedOperationException("method isnt implemented yet.");
+    }
+
+}
