cfdb99709a3bf59891ab91e2f8c9e67df14aa153
JuKu <kuenzel.justin@t-online.de>
2017-04-10 20:48:27 +0200

    changed ProjectileFactory.

--- game/src/main/java/dev/game/spacechaos/game/entities/factory/ProjectileFactory.java
+++ game/src/main/java/dev/game/spacechaos/game/entities/factory/ProjectileFactory.java
@@ -9,7 +9,21 @@ import dev.game.spacechaos.engine.entity.EntityManager;
  */
 public class ProjectileFactory {
 
-    public static Entity createProjectile (EntityManager ecs, float x, float y, Texture texture, float moveX, float moveY, float speed) {
+    /**
+    * create an new projectile entity
+     *
+     * @param ecs entity component system
+     * @param x x start position of projectile
+     * @param y y start position of projectile
+     * @param texture texture of projectile
+     * @param moveX x move direction (for example to move to left: x = -1, y = 0)
+     * @param moveY y move direction (for example to move to left: x = -1, y = 0)
+     * @param speed movement speed
+     * @param ttl time to live of projectile (after this time in milliseconds the entity will removed automatically)
+     *
+     * @return projectile entity
+    */
+    public static Entity createProjectile (EntityManager ecs, float x, float y, Texture texture, float moveX, float moveY, float speed, float ttl) {
         throw new UnsupportedOperationException("method isnt implemented yet.");
     }
 
