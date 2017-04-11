ddae6f891b1b912beaa2a7a6101ee825e80078ba
JuKu <kuenzel.justin@t-online.de>
2017-04-11 06:56:24 +0200

    added RangePoolFactory to recycle range objects.

---
+++ engine/src/main/java/dev/game/spacechaos/engine/collision/pool/RangePoolFactory.java
@@ -0,0 +1,40 @@
+package dev.game.spacechaos.engine.collision.pool;
+
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Pool;
+import dev.game.spacechaos.engine.collision.Range;
+
+/**
+ * Created by Justin on 11.04.2017.
+ */
+public class RangePoolFactory {
+
+    protected static Pool<Range> rangePool = null;
+
+    public static Pool<Range> createRectanglePool () {
+        if (rangePool == null) {
+            rangePool = new Pool<Range>() {
+
+                @Override
+                protected Range newObject () {
+                    return new Range();
+                }
+
+            };
+        }
+
+        return rangePool;
+    }
+
+    public static Pool<Range> createNewRectanglePool () {
+        return new Pool<Range>() {
+
+            @Override
+            protected Range newObject () {
+                return new Range();
+            }
+
+        };
+    }
+
+}
