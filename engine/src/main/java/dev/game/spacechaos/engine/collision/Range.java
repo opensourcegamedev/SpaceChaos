456d896a96c69027e3e4efce8176993985b4258e
JuKu <kuenzel.justin@t-online.de>
2017-04-11 06:29:32 +0200

    added Range.

---
+++ engine/src/main/java/dev/game/spacechaos/engine/collision/Range.java
@@ -0,0 +1,35 @@
+package dev.game.spacechaos.engine.collision;
+
+/**
+ * Created by Justin on 11.04.2017.
+ */
+public class Range {
+
+    protected float min = 0;
+    protected float max = 0;
+
+    public Range (float min, float max) {
+        set(min, max);
+    }
+
+    public float getMin () {
+        return this.min;
+    }
+
+    public float getMax () {
+        return this.max;
+    }
+
+    public void set (float min, float max) {
+        if (min > max) {
+            //swap values
+            float d = max;
+            max = min;
+            min = d;
+        }
+
+        this.min = min;
+        this.max = max;
+    }
+
+}
