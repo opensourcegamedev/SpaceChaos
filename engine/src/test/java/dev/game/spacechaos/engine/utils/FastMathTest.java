e0932928225512c2b901a084fa643db803490524
JuKu <kuenzel.justin@t-online.de>
2017-04-11 07:38:36 +0200

    added junit tests for FastMath.

---
+++ engine/src/test/java/dev/game/spacechaos/engine/utils/FastMathTest.java
@@ -0,0 +1,52 @@
+package dev.game.spacechaos.engine.utils;
+
+import com.badlogic.gdx.math.Vector2;
+import dev.game.spacechaos.engine.collision.Line;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Justin on 11.04.2017.
+ */
+public class FastMathTest {
+
+    @Test
+    public void testCheckVectorsParallel () {
+        Vector2 v1 = new Vector2(1, 1);
+        Vector2 v2 = new Vector2(1, 1);
+
+        assertEquals(true, FastMath.checkVectorsParallel(v1, v2));
+        assertEquals(true, FastMath.checkVectorsParallel(v2, v1));
+
+        Vector2 v3 = new Vector2(1, 2);
+        assertEquals(false, FastMath.checkVectorsParallel(v1, v3));
+        assertEquals(false, FastMath.checkVectorsParallel(v3, v1));
+
+        Vector2 v4 = new Vector2(2, 2);
+        assertEquals(true, FastMath.checkVectorsParallel(v1, v4));
+        assertEquals(true, FastMath.checkVectorsParallel(v4, v1));
+    }
+
+    @Test
+    public void testCheckIfLinesAreEquals () {
+        Line line1 = new Line(0, 0, 1, 1);
+        Line line2 = new Line(0, 0, 1, 1);
+
+        assertEquals(true, FastMath.checkIfLinesAreEquals(line1, line2));
+        assertEquals(true, FastMath.checkIfLinesAreEquals(line2, line1));
+
+        Line line3 = new Line(1, 1, 1, 1);
+        assertEquals(true, FastMath.checkIfLinesAreEquals(line1, line3));
+        assertEquals(true, FastMath.checkIfLinesAreEquals(line3, line1));
+
+        Line line4 = new Line(0, 0, -1, 1);
+        assertEquals(false, FastMath.checkIfLinesAreEquals(line1, line4));
+        assertEquals(false, FastMath.checkIfLinesAreEquals(line4, line1));
+
+        Line line5 = new Line(0, 0, 2, 2);
+        assertEquals(true, FastMath.checkIfLinesAreEquals(line1, line5));
+        assertEquals(true, FastMath.checkIfLinesAreEquals(line5, line1));
+    }
+
+}
