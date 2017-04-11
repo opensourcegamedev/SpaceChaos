24962a5f207eb2030e80981366fe388db9e15803
JuKu <kuenzel.justin@t-online.de>
2017-04-11 22:51:27 +0200

    added HUD overlay screen.

--- game/src/main/java/dev/game/spacechaos/game/Game.java
+++ game/src/main/java/dev/game/spacechaos/game/Game.java
@@ -5,6 +5,7 @@ import dev.game.spacechaos.engine.game.ScreenBasedGame;
 import dev.game.spacechaos.engine.screen.IScreen;
 import dev.game.spacechaos.engine.screen.ScreenManager;
 import dev.game.spacechaos.game.screen.GameScreen;
+import dev.game.spacechaos.game.screen.HUDOverlayScreen;
 
 /**
  * Created by Justin on 06.02.2017.
@@ -19,9 +20,13 @@ public class Game extends ScreenBasedGame {
     protected void onCreateScreens(ScreenManager<IScreen> screenManager) {
         //add game screen
         screenManager.addScreen("game", new GameScreen());
+        screenManager.addScreen("hud", new HUDOverlayScreen());
 
         //push screen
         screenManager.push("game");
+
+        //push HUD overlay screen (GUI)
+        screenManager.push("hud");
     }
 
 }
---
+++ game/src/main/java/dev/game/spacechaos/game/screen/HUDOverlayScreen.java
@@ -0,0 +1,34 @@
+package dev.game.spacechaos.game.screen;
+
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import dev.game.spacechaos.engine.game.ScreenBasedGame;
+import dev.game.spacechaos.engine.screen.impl.BaseScreen;
+import dev.game.spacechaos.engine.time.GameTime;
+
+/**
+ * Created by Justin on 11.04.2017.
+ */
+public class HUDOverlayScreen extends BaseScreen {
+
+    @Override
+    protected void onInit(ScreenBasedGame game, AssetManager assetManager) {
+
+    }
+
+    @Override
+    public void update(ScreenBasedGame game, GameTime time) {
+
+    }
+
+    @Override
+    public void draw(GameTime time, SpriteBatch batch) {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+}
