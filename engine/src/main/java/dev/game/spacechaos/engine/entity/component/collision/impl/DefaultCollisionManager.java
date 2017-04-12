fbc5f85070ef02f16e2aeb83ab8040a914ad5648
JuKu <kuenzel.justin@t-online.de>
2017-04-12 19:21:06 +0200

    changed DefaultCollisionManager, manager now gets all collision components from entities.

--- engine/src/main/java/dev/game/spacechaos/engine/entity/component/collision/impl/DefaultCollisionManager.java
+++ engine/src/main/java/dev/game/spacechaos/engine/entity/component/collision/impl/DefaultCollisionManager.java
@@ -3,9 +3,12 @@ package dev.game.spacechaos.engine.entity.component.collision.impl;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import dev.game.spacechaos.engine.camera.CameraWrapper;
+import dev.game.spacechaos.engine.entity.Entity;
 import dev.game.spacechaos.engine.entity.EntityManager;
+import dev.game.spacechaos.engine.entity.IComponent;
 import dev.game.spacechaos.engine.entity.component.collision.CollisionComponent;
 import dev.game.spacechaos.engine.entity.component.collision.CollisionManager;
+import dev.game.spacechaos.engine.entity.listener.ComponentListener;
 import dev.game.spacechaos.engine.time.GameTime;
 
 import java.util.ArrayList;
@@ -14,13 +17,14 @@ import java.util.List;
 /**
  * Created by Justin on 12.04.2017.
  */
-public class DefaultCollisionManager implements CollisionManager {
+public class DefaultCollisionManager implements CollisionManager, ComponentListener {
 
     //list with all collider components
     protected List<CollisionComponent> collisionComponentList = new ArrayList<>();
 
     public DefaultCollisionManager (EntityManager ecs) {
-        //
+        //register component listener
+        ecs.registerComponentListener(this);
     }
 
     @Override
@@ -28,6 +32,26 @@ public class DefaultCollisionManager implements CollisionManager {
         for (CollisionComponent component : this.collisionComponentList) {
             //draw collision shapes
             component.drawCollisionBoxes(time, camera, shapeRenderer, color);
+        }
+    }
+
+    @Override
+    public <T extends IComponent> void onComponentAdded(Entity entity, T component, Class<T> cls) {
+        if (component instanceof CollisionComponent) {
+            CollisionComponent collisionComponent = (CollisionComponent) component;
+
+            //add component to list
+            this.collisionComponentList.add(collisionComponent);
+        }
+    }
+
+    @Override
+    public <T extends IComponent> void onComponentRemoved(Entity entity, T component, Class<T> cls) {
+        if (component instanceof CollisionComponent) {
+            CollisionComponent collisionComponent = (CollisionComponent) component;
+
+            //remove component to list
+            this.collisionComponentList.remove(collisionComponent);
         }
     }
 
