3d2d2162a539110158aa041eb44cb931b13bf61c
JuKu <kuenzel.justin@t-online.de>
2017-04-12 21:23:44 +0200

    added support for collision detection for meteorits.

--- game/src/main/java/dev/game/spacechaos/game/entities/factory/MeteoritFactory.java
+++ game/src/main/java/dev/game/spacechaos/game/entities/factory/MeteoritFactory.java
@@ -1,11 +1,13 @@
 package dev.game.spacechaos.game.entities.factory;
 
 import com.badlogic.gdx.graphics.Texture;
+import dev.game.spacechaos.engine.collision.shape.CCircle;
 import dev.game.spacechaos.engine.entity.Entity;
 import dev.game.spacechaos.engine.entity.EntityManager;
 import dev.game.spacechaos.engine.entity.component.PositionComponent;
 import dev.game.spacechaos.engine.entity.component.ai.RandomWalkComponent;
 import dev.game.spacechaos.engine.entity.component.ai.SimpleFollowAIMovementComponent;
+import dev.game.spacechaos.engine.entity.component.collision.CollisionComponent;
 import dev.game.spacechaos.engine.entity.component.draw.DrawTextureComponent;
 import dev.game.spacechaos.engine.entity.component.draw.MoveDependentDrawRotationComponent;
 import dev.game.spacechaos.engine.entity.component.draw.SimpleRotationComponent;
@@ -32,6 +34,10 @@ public class MeteoritFactory {
 
         //add component so meteorits move randomly
         entity.addComponent(new RandomWalkComponent(), RandomWalkComponent.class);
+
+        //add collision component, so player can collide with meteorits
+        entity.addComponent(new CollisionComponent(), CollisionComponent.class);
+        entity.getComponent(CollisionComponent.class).addInnerShape(new CCircle(texture.getWidth() / 2, texture.getHeight() / 2, texture.getWidth() / 2));
 
         //add component to rotate entity
         entity.addComponent(new SimpleRotationComponent(1f), SimpleRotationComponent.class);
