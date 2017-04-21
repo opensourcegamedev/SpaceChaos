7de618d529c4ae858393a59acd83802ab8d07239
JuKu <kuenzel.justin@t-online.de>
2017-04-21 22:45:33 +0200

    added SpawnUtils.

---
+++ engine/src/main/java/dev/game/spacechaos/engine/utils/SpawnUtils.java
@@ -0,0 +1,35 @@
+package dev.game.spacechaos.engine.utils;
+
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by Justin on 21.04.2017.
+ */
+public class SpawnUtils {
+
+    protected static Vector2 tmpVector = new Vector2(0, 0);
+
+    public static Vector2 getRandomSpawnPosition (float minDistance, float maxDistance) {
+        if (minDistance < 0) {
+            throw new IllegalArgumentException("minDistance has to be >= 0.");
+        }
+
+        if (maxDistance < minDistance) {
+            throw new IllegalArgumentException("maxDistance has to be >= minDistance.");
+        }
+
+        //calculate new random distance
+        float randomDistance = (float) (Math.random() * (maxDistance - minDistance) + minDistance);
+
+        //get new random angle
+        float angle = (float) Math.random() * 360;
+
+        //set length and angle of vector
+        tmpVector.set(randomDistance, 0);
+        tmpVector.setLength(randomDistance);
+        tmpVector.setAngle(angle);
+
+        return tmpVector;
+    }
+
+}
