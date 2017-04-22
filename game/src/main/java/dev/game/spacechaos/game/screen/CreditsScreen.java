692f773c050602c6af7e7a599b88622767bb7b84
JuKu <kuenzel.justin@t-online.de>
2017-04-22 18:02:00 +0200

    added beginning of credits screen, see #40 .

---
+++ game/src/main/java/dev/game/spacechaos/game/screen/CreditsScreen.java
@@ -0,0 +1,45 @@
+package dev.game.spacechaos.game.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import dev.game.spacechaos.engine.font.BitmapFontFactory;
+import dev.game.spacechaos.engine.game.ScreenBasedGame;
+import dev.game.spacechaos.engine.screen.impl.BaseScreen;
+import dev.game.spacechaos.engine.time.GameTime;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Timer;
+
+/**
+ * Created by Justin on 22.04.2017.
+ */
+public class CreditsScreen extends BaseScreen {
+
+    @Override
+    protected void onInit(ScreenBasedGame game, AssetManager assetManager) {
+
+    }
+
+    @Override
+    public void update(ScreenBasedGame game, GameTime time) {
+        //
+    }
+
+    @Override
+    public void draw(GameTime time, SpriteBatch batch) {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+}
