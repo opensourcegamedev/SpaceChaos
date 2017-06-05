c425c1e73777713e9ed42abb658714436994cfb9
crykn <crykn@users.noreply.github.com>
2017-06-06 01:12:20 +0200

    Added technical part of ammo crates.

---
+++ game/src/main/java/dev/game/spacechaos/game/entities/component/powerup/TorpedoAmmoCrateComponent.java
@@ -0,0 +1,33 @@
+package dev.game.spacechaos.game.entities.component.powerup;
+
+import dev.game.spacechaos.engine.entity.Entity;
+import dev.game.spacechaos.game.entities.component.combat.WeaponInventoryComponent;
+
+/**
+ * Adds an ammo crate for torpedos effect to entities.
+ *
+ * @author SpaceChaos-Team
+ *         (https://github.com/opensourcegamedev/SpaceChaos/blob/master/CONTRIBUTORS.md)
+ * @since 1.0.2-PreAlpha
+ */
+public class TorpedoAmmoCrateComponent extends BasePowerupComponent {
+
+    private int ammo;
+
+    public TorpedoAmmoCrateComponent(int ammo) {
+        this.ammo = ammo;
+    }
+
+    @Override
+    protected boolean onEffect(Entity affectedEntity) {
+        WeaponInventoryComponent weaponInvComp = affectedEntity.getComponent(WeaponInventoryComponent.class);
+
+        if (weaponInvComp != null) {
+            weaponInvComp.addAmmoToRightWeapon(ammo);
+            return true;
+        }
+
+        return false;
+    }
+
+}
