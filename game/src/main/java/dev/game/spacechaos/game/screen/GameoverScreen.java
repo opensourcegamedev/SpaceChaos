6bda2f114e01230cff961b8f0c4112fd02418d45
crykn <crykn@users.noreply.github.com>
2017-07-04 22:11:25 +0200

    Small improvements to the design of the game over screen.

--- game/src/main/java/dev/game/spacechaos/game/screen/GameoverScreen.java
+++ game/src/main/java/dev/game/spacechaos/game/screen/GameoverScreen.java
@@ -11,6 +11,7 @@ import dev.game.spacechaos.engine.font.BitmapFontFactory;
 import dev.game.spacechaos.engine.game.ScreenBasedGame;
 import dev.game.spacechaos.engine.hud.widgets.ColoredTextButton;
 import dev.game.spacechaos.engine.hud.widgets.ImageButton;
+import dev.game.spacechaos.engine.hud.widgets.TextButton;
 import dev.game.spacechaos.engine.screen.impl.BaseScreen;
 import dev.game.spacechaos.engine.sound.VolumeManager;
 import dev.game.spacechaos.engine.time.GameTime;
@@ -28,9 +29,6 @@ public class GameoverScreen extends BaseScreen {
     private static final String BG_IMAGE_PATH = "./data/wallpaper/galaxy1/galaxy1.png";
     private static final String GAMEOVER_SOUND_PATH = "./data/sound/rock_breaking/rock_breaking.ogg";
 
-    private static final String BUTTON_IMAGE_PATH = "./data/images/gui/restart_button.png";
-    private static final String BUTTON_HOVER_PATH = "./data/images/gui/restart_button_hovered.png";
-
     // font for buttons & text
     private BitmapFont font = null;
     private BitmapFont buttonFont = null;
@@ -39,9 +37,7 @@ public class GameoverScreen extends BaseScreen {
     private Sound sound = null;
 
     // private ColoredTextButton replayButton = null;
-    private Texture buttonTexture = null;
-    private Texture hoverTexture = null;
-    private ImageButton replayButton = null;
+    private TextButton replayButton = null;
 
     private String timeText = "";
     private String scoreText = "";
@@ -55,24 +51,15 @@ public class GameoverScreen extends BaseScreen {
         game.getAssetManager().finishLoadingAsset(BG_IMAGE_PATH);
         sound = game.getAssetManager().get(GAMEOVER_SOUND_PATH);
         this.bgTexture = game.getAssetManager().get(BG_IMAGE_PATH);
-
-        // load & get button textures
-        game.getAssetManager().load(BUTTON_IMAGE_PATH, Texture.class);
-        game.getAssetManager().load(BUTTON_HOVER_PATH, Texture.class);
-        game.getAssetManager().finishLoadingAsset(BUTTON_IMAGE_PATH);
-        game.getAssetManager().finishLoadingAsset(BUTTON_HOVER_PATH);
-        this.buttonTexture = game.getAssetManager().get(BUTTON_IMAGE_PATH, Texture.class);
-        this.hoverTexture = game.getAssetManager().get(BUTTON_HOVER_PATH, Texture.class);
-
         // generate fonts
         this.font = BitmapFontFactory.createFont("./data/font/spartakus/SparTakus.ttf", 48, Color.WHITE, Color.BLUE, 3);
-        this.buttonFont = BitmapFontFactory.createFont("data/font/arial/arial.ttf", 32, Color.RED);
+        this.buttonFont = BitmapFontFactory.createFont("data/font/arial/arial.ttf", 32, Color.WHITE);
 
-        this.replayButton = new ImageButton(this.buttonTexture, this.hoverTexture);
-        this.replayButton.setDimension(/* 200 */400, /* 50 */128);
-        this.replayButton.setPosition(game.getViewportWidth() / 2 - (replayButton.getWidth() / 2), 128);
+        this.replayButton = new TextButton("Zum Menu", buttonFont);
+        this.replayButton.setDimension(/* 200 */400, /* 50 */50);
+        this.replayButton.setPosition(game.getViewportWidth() / 2 - (replayButton.getWidth() / 2), 260);
 
-        this.replayButton.setClickListener(() -> game.getScreenManager().leaveAllAndEnter("game"));
+        this.replayButton.setClickListener(() -> game.getScreenManager().leaveAllAndEnter("menu"));
     }
 
     @Override
@@ -100,10 +87,10 @@ public class GameoverScreen extends BaseScreen {
 
         batch.draw(this.bgTexture, 0, 0);
 
-        this.font.draw(batch, "GAME OVER", game.getViewportWidth() / 2 - 200, game.getViewportHeight() / 2);
+        this.font.draw(batch, "GAME OVER", game.getViewportWidth() / 2 - 245, game.getViewportHeight() / 2 + 70);
 
-        this.buttonFont.draw(batch, "Elapsed Time: " + this.timeText, 100, 100);
-        this.buttonFont.draw(batch, "Score: " + this.scoreText, 100, 50);
+        this.buttonFont.draw(batch, "Elapsed Time: " + this.timeText, 75, 120);
+        this.buttonFont.draw(batch, "Score: " + this.scoreText, 75, 70);
 
         // draw replay button
         this.replayButton.drawLayer0(time, batch);
